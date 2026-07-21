220907402eedbf84510ec21ad1b961b43558b1b5
Sorrawit Amdaeng <bank23113@gmail.com>
2026-07-22 00:44:17 +0700

    feat(validation): add class-transformer and class-validator dependencies; implement global validation and serialization in main application

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
-import { NestFactory } from '@nestjs/core';
+import {
+  ClassSerializerInterceptor,
+  INestApplication,
+  ValidationPipe,
+} from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+
+function registerGlobals(app: INestApplication) {
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      whitelist: true,
+    }),
+  );
+  app.useGlobalInterceptors(
+    new ClassSerializerInterceptor(app.get(Reflector), {
+      excludeExtraneousValues: true,
+    }),
+  );
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  registerGlobals(app);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
