9726d8a9e6f1e76d829f16afae0ec028cfd231ee
Sorrawit Amdaeng <bank23113@gmail.com>
2026-07-24 20:25:00 +0700

    feat(validation): enhance global validation with custom exception handling for validation errors

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import {
+  BadRequestException,
   ClassSerializerInterceptor,
+  HttpStatus,
   INestApplication,
   ValidationPipe,
 } from '@nestjs/common';
@@ -9,6 +11,19 @@ import { AppModule } from './app.module';
 function registerGlobals(app: INestApplication) {
   app.useGlobalPipes(
     new ValidationPipe({
+      exceptionFactory: (errors) => {
+        const fields = errors.map((error) => error.property);
+        const message = errors
+          .flatMap((error) => Object.values(error.constraints ?? {}))
+          .map((m) => m.slice(0, 1).toUpperCase() + m.slice(1))
+          .join('. ');
+        return new BadRequestException({
+          message,
+          fields,
+          error: 'Bad Request',
+          statusCode: HttpStatus.BAD_REQUEST,
+        });
+      },
       forbidNonWhitelisted: true,
       transform: true,
       transformOptions: {
