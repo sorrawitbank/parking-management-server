8ae9b4a67d4eefc5db23824becb67154ce6e23d7
Sorrawit Amdaeng <bank23113@gmail.com>
2026-07-30 00:28:23 +0700

    refactor(database): consolidate schema imports into a single schema object for improved structure

--- src/database/types/database.d.ts
+++ src/database/types/database.d.ts
@@ -1,6 +1,12 @@
 import { NodePgDatabase } from 'drizzle-orm/node-postgres';
+import * as relations from '../database.relations';
 import * as schemas from '../database.schemas';
 
-type Database = NodePgDatabase<typeof schemas>;
+const schema = {
+  ...relations,
+  ...schemas,
+};
+
+type Database = NodePgDatabase<typeof schema>;
 
 export default Database;
