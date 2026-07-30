ce453595628b787edb7c1ed0e5567f3c3d167c04
Sorrawit Amdaeng <bank23113@gmail.com>
2026-07-30 15:32:00 +0700

    refactor(provinces): enhance service and DTO validation with improved error handling and export functionality

--- src/provinces/dto/province-id.dto.ts
+++ src/provinces/dto/province-id.dto.ts
@@ -1,8 +1,10 @@
-import { IsInt, Max, Min } from 'class-validator';
+import { IsInt, IsNotEmpty, Max, Min } from 'class-validator';
+import ErrorType from '../../common/errors/error-type';
 
 export class ProvinceIdDto {
-  @IsInt()
-  @Min(10)
-  @Max(99)
+  @Max(99, { message: ErrorType.ABOVE_MAXIMUM })
+  @Min(10, { message: ErrorType.BELOW_MINIMUM })
+  @IsInt({ message: ErrorType.INVALID_TYPE })
+  @IsNotEmpty({ message: ErrorType.REQUIRED })
   readonly provinceId: number;
 }
--- src/provinces/provinces.module.ts
+++ src/provinces/provinces.module.ts
@@ -7,5 +7,6 @@ import { DatabaseModule } from '../database/database.module';
   imports: [DatabaseModule],
   controllers: [ProvincesController],
   providers: [ProvincesService],
+  exports: [ProvincesService],
 })
 export class ProvincesModule {}
