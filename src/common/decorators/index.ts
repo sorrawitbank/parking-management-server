6a5c02a7477885a0f6127e3b9a9b87977fe4dbc2
Sorrawit Amdaeng <bank23113@gmail.com>
2026-08-23 17:14:15 +0700

    feat(decorators): add ParseStringArray decorator to transform comma-separated strings into unique string arrays

--- src/common/decorators/index.ts
+++ src/common/decorators/index.ts
@@ -1,2 +1,3 @@
 export { default as ParseNumberArray } from './parse-number-array.decorator';
+export { default as ParseStringArray } from './parse-string-array.decorator';
 export { default as Trim } from './trim.decorator';
---
+++ src/common/decorators/parse-string-array.decorator.ts
@@ -0,0 +1,23 @@
+import { Transform } from 'class-transformer';
+
+/**
+ * Parse a comma-separated string into an array of unique strings
+ *
+ * @returns The array of unique strings
+ * @example 'a, b, b, c, d, d, e' => ['a', 'b', 'c', 'd', 'e']
+ */
+function ParseStringArray() {
+  return Transform(({ value }) =>
+    value
+      ? [
+          ...new Set(
+            String(value)
+              .split(',')
+              .map((val) => val.trim()),
+          ),
+        ]
+      : undefined,
+  );
+}
+
+export default ParseStringArray;
